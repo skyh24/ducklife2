7d9730262074b879507cfb2fe8b19c2ae4798ba7
skyh24 <931998156@qq.com>
2015-01-11 00:07:48 +0800

    1.5 weixin

--- base/weixin.py
+++ base/weixin.py
@@ -1,25 +1,61 @@
 import urllib, urllib2
 import hashlib, json
 from django.http import HttpResponse
-def accessInterface(request):
-    echostr = request.GET.get('echostr', '')
-    timestamp = request.GET.get('timestamp', '')
-    nonce = request.GET.get('nonce', '')
-    signature = request.GET.get('signature', '')
-    if checkSignature(signature, 'xiaoxian', timestamp, nonce):
-        return HttpResponse(echostr)
-    else:
-        return HttpResponse('Can not access Weixin interface')
+from django.utils.encoding import smart_str, smart_unicode 
+import xml.etree.ElementTree as ET  
 
-def checkSignature(signature, token, timestamp, nonce):
-    tmpArr = [token, timestamp, nonce]
-    tmpArr = sorted(tmpArr)
-    tmpStr = ''.join(tmpArr)
-    sha1Str = hashlib.sha1(tmpStr).hexdigest()
-    if signature == sha1Str:
-        return True
-    else:
-        return False
+TOKEN = "xiaoxian"
+
+def accessInterface(request):
+    if request.method == 'GET':
+        response = HttpResponse(checkSignature(request),content_type="text/plain")
+        return response
+    if request.method == 'POST':
+        response = HttpResponse(responseMsg(request),content_type="application/xml")
+        return response
+    else
+        return "can not access"
+
+def checkSignature(request):
+    global TOKEN
+    echostr = request.GET.get('echostr', None)
+    timestamp = request.GET.get('timestamp', None)
+    nonce = request.GET.get('nonce', None)
+    signature = request.GET.get('signature', None)
+    token = TOKEN
+
+    tmpList = [token,timestamp,nonce]  
+    tmpList.sort()  
+    tmpstr = "%s%s%s" % tuple(tmpList) 
+    tmpstr = hashlib.sha1(tmpstr).hexdigest()  
+    if tmpstr == signature:  
+        return echoStr  
+    else:  
+        return "can not access wechat"  
+
+
+def responseMsg(request):
+    xmlstr = smart_str(request.body)
+    xml = ET.fromstring(xmlstr)
+
+    ToUserName = xml.find('ToUserName').text
+    FromUserName = xml.find('FromUserName').text
+    CreateTime = xml.find('CreateTime').text
+    MsgType = xml.find('MsgType').text
+    Content = xml.find('Content').text
+    MsgId = xml.find('MsgId').text
+    if Content == ''
+        Content = 'welcome'
+    reply_xml = """<xml>
+       <ToUserName><![CDATA[%s]]></ToUserName>
+       <FromUserName><![CDATA[%s]]></FromUserName>
+       <CreateTime>%s</CreateTime>
+       <MsgType><![CDATA[text]]></MsgType>
+       <Content><![CDATA[%s]]></Content>
+       </xml>"""%(FromUserName,ToUserName,CreateTime,Content + "  Hello world, this is test message")
+
+    return HttpResponse(reply_xml)
+
 
 def getOpenid(appid, appsecret, code):
     url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=' + \
