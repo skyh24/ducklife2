ccc246aebb7c99ff1719b534539518c1077c2aad
skyh24 <931998156@qq.com>
2015-01-11 21:23:50 +0800

    1.2 little bug

--- product/urls.py
+++ product/urls.py
@@ -8,7 +8,7 @@ import settings
 urlpatterns = patterns('product.views',
     url(r'^$', 'index_view', {'templateName' : 'index.html'}),
     url(r'^cart/$', 'cart_view', {'templateName' : 'cart.html'}),
-    url(r'^kefu/$', 'kefu_view', {'templateName' : 'kefu.html'}),
+    url(r'^kefu/$', 'kefu_view'),
     url(r'^(\d+)/product/$', 'product_view', {'templateName' : 'product.html'}),
     url(r'^product/(\d+)/$', 'product_detail_view', {'templateName' : 'product_detail.html'}),
     url(r'^clear/$' ,'clear_cart'), # for ajax
